a0f9b634f3ecaac6fa364d18380928ad5ba57637
Emilio Delgado <emilio05@gmail.com>
2018-10-17 20:42:35 -0300

    Agrego ConnectionFactory

---
+++ src/dao/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	private static ConnectionFactory instancia;
+	//String connectionUrl = "jdbc:sqlserver://bd;databaseName=TPO_AI;user=AI_4084_08;password=AI_4084_08";//para ejecución en Laboratorio
+	String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=TPO_AI;User=AI_4084_08;Password=AI_4084_08";//para ejecución en localhost
+	
+	private ConnectionFactory() throws ClassNotFoundException{
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	}
+	
+	public static ConnectionFactory getInstancia() throws ClassNotFoundException{
+		if(instancia == null)
+			instancia = new ConnectionFactory();
+		return instancia;
+	}
+	
+	public Connection getConection() throws SQLException{
+		return DriverManager.getConnection(connectionUrl);
+	}
+}
