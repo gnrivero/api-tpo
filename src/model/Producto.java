b54752339ac8bf16c91f9cc3547d0c33ea9c48af
Maria <marisaqp@gmail.com>
2018-10-17 21:16:45 -0300

    Update src/model/Producto.java

--- src/model/Producto.java
+++ src/model/Producto.java
@@ -1,5 +1,47 @@
 package model;
 
+/**
+ * @author Maria
+ *
+ */
 public class Producto {
 
+	private int idProducto;
+	private int codigo;
+	private String titulo;
+	private String Descripcion;
+	private float precio;
+	public int getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
+	
 }
