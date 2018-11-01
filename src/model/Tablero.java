e88b80b85d4e8eb9d2c4304307aaad86f42c4ccc
Maria <marisaqp@gmail.com>
2018-10-31 21:13:26 -0300

    Agrego cosas al tablero
    Bla bla

--- src/model/Tablero.java
+++ src/model/Tablero.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.List;
+
+import model.reclamo.Reclamo;
+
 public class Tablero {
+	
+	private String nombre;
+	private List <Reclamo> reclamos;
+	
+	public List <Reclamo> cargarReclamo ()
+	{
+		return reclamos;
+		
+	}
+	public void agregarReclamo(Reclamo reclamo)
+	{
+		
+	}
 
 }
