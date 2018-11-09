bdeb7809131f00d873517f630bcafcc1f7c74278
gnrivero <31462340+gnrivero@users.noreply.github.com>
2018-11-08 22:39:22 -0300

    Corrijo errores en cambio de paquete

--- src/gui/forms/JFormTablero.java
+++ src/gui/forms/JFormTablero.java
@@ -1,4 +1,4 @@
-package vista.forms;
+package gui.forms;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/gui/forms/TableroGui.java
+++ src/gui/forms/TableroGui.java
@@ -1,4 +1,4 @@
-package vista.forms;
+package gui.forms;
 
 import java.util.ArrayList;
 import java.util.List;
